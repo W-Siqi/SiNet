60f4b35b00f30968e71aeb75c43eb62609c21d03
SiqiWang <wangsiqi_mailbox@163.com>
2020-02-04 00:25:13 -0500

    movement sys

---
+++ Resources/Doc/Devlog.md
@@ -0,0 +1,24 @@
+# Ver 1.0
+1. synchonzing and replicate functions
+2. server side backend 
+3. application layer based on TCP
+4. establish the demo scene and github repo
+
+# Ver 1.1 - (doing)
+1. refactor, using ECS architecual mode. 
+2. movement lerping system
+1. log system for analyze data receiving
+9. visual log: move ment, sync chonize.
+
+
+# To-Do list
+1. provide 'client-as-server' throug LAN
+3. handle diffent connection lost and reconnection cases 
+2. robast and effient application layer protocal
+3. RPC mechansim
+4. provide the matching system server in 'client-as-server' mode
+5. editor in unity to encapsule operations on entity&components
+6. Tech demos, Prefabs 
+7. bandwidth optimidze(only sync the changed data necessary)
+8. animation sync plan 
+10. pressure tester. see what happen when latency is over like 500ms
---
+++ SiNetUnity/Assets/Scripts/ECS/MovementSystem.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SiNet {
+    public class MovementSystem : MonoBehaviour
+    {
+        const float ENTITY_LIST_UPDATE_INTERVAL = 1f;
+
+        // MovementSystem will create and update the MovementEnity
+        // The real-time position caltucated by MovementEntity
+        private class MovementEntity {
+            public SyncEntity entity;
+            public SyncTransform syncTransform;
+
+            private float lastUpdateTime;
+            private Vector3 lastPos;
+
+            public bool isValid { get { return entity != null && syncTransform != null; } }
+
+            public MovementEntity(SyncEntity entity, SyncTransform transform) {
+                this.entity = entity;
+                this.syncTransform = transform;
+            }
+
+            // TBD: now the way calculation position is a placeholder
+            public void UpdateMovement() {
+                if (isValid) {
+                    entity.transform.position = syncTransform.position;
+                    entity.transform.rotation = syncTransform.rotation;
+                }
+            }
+
+            private MovementEntity() { }
+        }
+
+        private List<MovementEntity> targets= new List<MovementEntity>();
+
+        private void Update()
+        {
+            UpdateMovement();
+        }
+
+        private void Start() {
+            StartCoroutine(EntitiesUpdateLoop());
+        }
+
+        // if new entities added, it will be detected at next loop
+        // it will also clear the invalid entities 
+        IEnumerator EntitiesUpdateLoop() {
+            while (true) {
+                UpdateEntityList();
+                yield return new WaitForSeconds(ENTITY_LIST_UPDATE_INTERVAL);
+            }
+        }
+
+        private void UpdateEntityList() {
+            // clear old entity
+            var toRemove = new List<MovementEntity>();
+            foreach (var tar in targets)
+                if (!tar.isValid)
+                    toRemove.Add(tar);
+            foreach (var tar in toRemove)
+                targets.Remove(tar);
+
+
+            // check new entity
+            foreach (var entity in GameObject.FindObjectsOfType<SyncEntity>())
+            {
+                if (IsMovementEntity(entity) && !IsInTargets(entity))
+                {
+                    var newTarget = new MovementEntity(entity, entity.GetComponent<SyncTransform>());
+                    targets.Add(newTarget);
+                }                   
+            }
+        }
+
+        private bool IsInTargets(SyncEntity entity) {
+            foreach(var tar in targets)
+            {
+                if (tar.entity == entity)
+                    return true;
+            }
+            return false;
+        }
+
+        // defineation of movment entity is:
+        // 1-have syncTransfrom component
+        // 2-isn't local authority
+        private bool IsMovementEntity(SyncEntity entity) {
+            return entity.authorityType != SyncEntity.AuthorityType.local
+                    && entity.GetComponent<SyncTransform>();
+        }
+
+        // envoke exsited movement entity to caltulate lastest position
+        private void UpdateMovement() {
+            foreach (var tar in targets) {
+                if (tar.isValid)
+                    tar.UpdateMovement();
+            }
+        }
+    }
+}
