d10f09544a1a7ff69c0b221a1b1493052dafafb8
SiqiWang <wangsiqi_mailbox@163.com>
2020-02-09 22:15:50 -0500

    behavirou tree fundation

--- Resources/Doc/Devlog.md
+++ Resources/Doc/Devlog.md
@@ -6,10 +6,14 @@
 
 # Ver 1.1 - (doing)
 1. refactor, using ECS architecual mode. 
+    - message collector 
+    - entity manager
 2. movement lerping system
+3. roboast application layer 
 1. log system for analyze data receiving
-9. visual log: move ment, sync chonize.
 
+# Ver 1.2 
+1. VR characer present scheme
 
 # To-Do list
 1. provide 'client-as-server' throug LAN
@@ -22,3 +26,4 @@
 7. bandwidth optimidze(only sync the changed data necessary)
 8. animation sync plan 
 10. pressure tester. see what happen when latency is over like 500ms
+11. visual log: move ment, sync chonize.
--- SiNetUnity/Assets/Samples/VRCharacterPresent/AI/WolfKingBehaviours/LeafNodes/CircleRun.cs
+++
@@ -1,29 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;namespace BehaviourTree {
-    public class CircleRun : WolfKingLeafNode
-    {
-        protected override IEnumerator NodeRunning()
-        {
-            _state = State.running;
-
-            var trackInfo = stateTable.navigator.RunRouteFromStartToEnd(
-                stateTable.circleRoute, 
-                stateTable.attribute.runningSpeed,
-                stateTable.attribute.trunningAngleSpeed);
-
-            while (!trackInfo.isFinished)
-            {
-                if (stateTable.isStunned) {
-                    stateTable.navigator.StopNavigating();
-                    break;
-                }
-                yield return null;
-            }
-
-            if (stateTable.isStunned)
-                _state = State.fail;
-            else
-                _state = State.success;
-        }
-    }}
