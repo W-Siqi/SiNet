0d2ee11ce341bce527a22cd92ac6d49df35a3125
SiqiWang <35263331+W-Siqi@users.noreply.github.com>
2020-02-11 19:40:54 -0500

    setup VR Character present demo scene

--- SiNetUnity/Assets/Samples/DoublePerpective/CharacterController.cs
+++ SiNetUnity/Assets/Samples/DoublePerpective/CharacterController.cs
@@ -2,27 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
-{
-    public float movementSpeed = 10;
-
-    private Vector3 forward {
-        get {
-            if (!ViewCam.instance) return Vector3.forward;
-
-            var forward = transform.position - ViewCam.instance.realCamPos;
-            forward.y = 0;
-            forward.Normalize();
-            return forward;
-        }
-    }
-
-
-    // Update is called once per frame
-    void Update()
+namespace SiNet.Utils{
+    public class CharacterController : MonoBehaviour
     {
-        transform.rotation = Quaternion.LookRotation(forward);
-        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        transform.Translate(0, 0, vertical);
+        public float movementSpeed = 10;
+
+        private Vector3 forward
+        {
+            get
+            {
+                if (!ViewCam.instance) return Vector3.forward;
+
+                var forward = transform.position - ViewCam.instance.realCamPos;
+                forward.y = 0;
+                forward.Normalize();
+                return forward;
+            }
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.rotation = Quaternion.LookRotation(forward);
+            float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+            transform.Translate(0, 0, vertical);
+        }
     }
 }
---
+++ SiNetUnity/Assets/Samples/VRCharacterPresent/Scripts/PlayerSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelector : MonoBehaviour
+{
+    public GameObject UIMenu;
+    public GameObject nursePlayerPrefab;
+    public GameObject patientPlayerPrefab;
+    public GameObject observerPrefab;
+
+    public void SelectNurse() {
+        Instantiate(nursePlayerPrefab);
+        UIMenu.SetActive(false);
+    }
+
+    public void SelectPatient()
+    {
+        Instantiate(patientPlayerPrefab);
+        UIMenu.SetActive(false);
+    }
+
+    public void SelectObserver()
+    {
+        Instantiate(observerPrefab);
+        UIMenu.SetActive(false);
+    }
+}
